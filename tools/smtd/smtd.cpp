2a7a342e44b372a8204a15f7739c40d9ff0b2e1f
qingkaishi <qingkaishi@gmail.com>
2016-12-20 14:17:50 +0800

    refactoring

--- tools/smtd/smtd.cpp
+++ tools/smtd/smtd.cpp
@@ -21,6 +21,7 @@
 
 #include "Support/SignalHandler.h"
 #include "Support/MessageQueue.h"
+#include "UserIDAllocator.h"
 #include "SMT/SMTFactory.h"
 
 #define DEBUG_TYPE "smtd"
@@ -31,7 +32,7 @@ static cl::opt<int> MSQKey("smtd-key", cl::desc("Indicate the key of the master'
 
 static cl::opt<bool> Incremental("smtd-incremental", cl::desc("Enable incremental SMT solving."), cl::init(false));
 
-static cl::opt<bool> TestClient("smtd-test", cl::desc("Run a testing client."), cl::init(false), cl::ReallyHidden);
+static cl::opt<bool> RunTestClient("smtd-test", cl::desc("Run a testing client."), cl::init(false), cl::ReallyHidden);
 
 // a testing client
 extern void test(int Key);
@@ -42,7 +43,7 @@ static MessageQueue* CommandMSQ = nullptr;
 
 static MessageQueue* CommunicateMSQ = nullptr;
 
-static pid_t MainProcessId;
+static pid_t MainProcessID;
 
 int main(int argc, char **argv) {
     PrettyStackTraceProgram X(argc, argv);
@@ -53,26 +54,22 @@ int main(int argc, char **argv) {
 
     cl::ParseCommandLineOptions(argc, argv, "SMT solving service.\n");
 
-    if (TestClient.getValue()) {
+    if (RunTestClient.getValue()) {
         test(MSQKey.getValue());
         return 0;
     }
 
-    outs() << "*******************************\n"
-            << "Please run your applications with -solver-enable-smtd=" << MSQKey.getValue()
-            << " -solver-enable-smtd-incremental=" << (Incremental.getValue() ? "true" : "false") << "\n"
-            << "*******************************\n";
-
     if (Incremental.getValue()) {
         llvm_unreachable("Incremental mode has not been supported yet!");
+        abort();
     }
 
     // Signal handlers
-    MainProcessId = getpid();
+    MainProcessID = getpid();
     RegisterSignalHandler();
     std::function<void()> ExitHandler = []() {
         // close all child processes
-        if (MainProcessId == getpid()) {
+        if (MainProcessID == getpid()) {
             CommandMSQ->destroy();
             CommunicateMSQ->destroy();
             delete CommunicateMSQ;
@@ -97,6 +94,11 @@ int main(int argc, char **argv) {
     AddInterruptSigHandler(ExitHandler);
     AddErrorSigHandler(ExitHandler);
 
+    outs() << "*******************************\n"
+           << "Please run your applications with -solver-enable-smtd=" << MSQKey.getValue()
+           << " -solver-enable-smtd-incremental=" << (Incremental.getValue() ? "true" : "false") << "\n"
+           << "*******************************\n";
+
     UserIDAllocator* IDAllocator = UserIDAllocator::getUserAllocator();
     std::vector<std::pair<pid_t, int>> FreeMSQs;
     std::map<long, std::pair<pid_t, int>> UserWorkerMap;
@@ -106,18 +108,18 @@ int main(int argc, char **argv) {
     CommandMSQ = new MessageQueue(MSQKey.getValue(), true);
     CommunicateMSQ = new MessageQueue(MSQKey.getValue() + ++Counter, true);
 
-    std::string Command, CtrlMsg;
+    std::string Command, CtrlMsg; // buffer that can be reused
     while (true) {
         if (-1 == CommandMSQ->recvMessage(Command)) {
             llvm_unreachable("[Master] fail to recv command");
         }
         DEBUG(errs() << "[Master] get msg (0): " << Command << "\n";);
 
-        long UserId = -1;
+        long UserID = -1;
         try {
             if (Command == "requestid") {
-                UserId = IDAllocator->allocate();
-                CommunicateMSQ->sendMessage(std::to_string(UserId), 12);
+                UserID = IDAllocator->allocate();
+                CommunicateMSQ->sendMessage(std::to_string(UserID), 12);
                 continue;
             }
 
@@ -127,27 +129,27 @@ int main(int argc, char **argv) {
             }
 
             // request message: "user_id:request_content";
-            UserId = std::stol(Command.substr(0, M));
-            if (UserId < 100) {
-                throw std::runtime_error("[Master] UserId cannot less than 100!");
+            UserID = std::stol(Command.substr(0, M));
+            if (UserID < 100) {
+                throw std::runtime_error("[Master] UserID cannot be less than 100!");
             }
 
             std::string UserRequest = Command.substr(M + 1);
             DEBUG(errs() << "[Master] get request: " << UserRequest << "\n";);
             if (UserRequest == "open") {
-                auto It = UserWorkerMap.find(UserId);
+                auto It = UserWorkerMap.find(UserID);
                 if (It == UserWorkerMap.end()) {
                     if (!FreeMSQs.empty()) {
                         // try to reuse
                         auto& Worker = FreeMSQs.back();
 
-                        DEBUG(errs() << "[Master] (open) Reusing and sending to " << UserId << ": " << Worker.second << "...");
-                        if (CommunicateMSQ->sendMessage(std::to_string(Worker.second), UserId) == -1) {
+                        DEBUG(errs() << "[Master] (open) Reusing and sending to " << UserID << ": " << Worker.second << "...");
+                        if (CommunicateMSQ->sendMessage(std::to_string(Worker.second), UserID) == -1) {
                             throw std::runtime_error("[Master] Fail to send slave id to user after open with reuse!");
                         }
                         DEBUG(errs() << "Done!\n");
 
-                        UserWorkerMap[UserId] = Worker;
+                        UserWorkerMap[UserID] = Worker;
                         FreeMSQs.pop_back();
 
                         // Here we need one guarantee
@@ -162,11 +164,11 @@ int main(int argc, char **argv) {
                     throw std::runtime_error("[Master] Existent user requests open!");
                 }
             } else if (UserRequest == "close") {
-                auto It = UserWorkerMap.find(UserId);
+                auto It = UserWorkerMap.find(UserID);
                 if (It != UserWorkerMap.end()) {
                     UserWorkerMap.erase(It);
                     FreeMSQs.push_back(It->second);
-                    IDAllocator->recycle(UserId);
+                    IDAllocator->recycle(UserID);
                     continue;
                 } else {
                     throw std::runtime_error("[Master] Non-existent user requests close!");
@@ -174,7 +176,7 @@ int main(int argc, char **argv) {
             } else if (UserRequest == "reopen") {
                 // reopen means the client detects the exception of
                 // the slave, and request a new one.
-                auto It = UserWorkerMap.find(UserId);
+                auto It = UserWorkerMap.find(UserID);
                 if (It != UserWorkerMap.end()) {
                     auto ChildPID = It->second.first;
                     UserWorkerMap.erase(It);
@@ -184,13 +186,13 @@ int main(int argc, char **argv) {
                     // try reuse
                     if (!FreeMSQs.empty()) {
                         auto& Worker = FreeMSQs.back();
-                        DEBUG(errs() << "[Master] (reopen) Reusing and sending to " << UserId << ": " << Worker.second << "...");
-                        if (CommunicateMSQ->sendMessage(std::to_string(Worker.second), UserId) == -1) {
+                        DEBUG(errs() << "[Master] (reopen) Reusing and sending to " << UserID << ": " << Worker.second << "...");
+                        if (CommunicateMSQ->sendMessage(std::to_string(Worker.second), UserID) == -1) {
                             throw std::runtime_error("[Master] Fail to send slave id to user after reopen!");
                         }
                         DEBUG(errs() << "Done!\n");
 
-                        UserWorkerMap[UserId] = Worker;
+                        UserWorkerMap[UserID] = Worker;
                         FreeMSQs.pop_back();
 
                         // Here we need one guarantee
@@ -216,8 +218,8 @@ int main(int argc, char **argv) {
         int ChildMSQKey = MSQKey.getValue() + ++Counter;
         SlaveMSQ = new MessageQueue(ChildMSQKey, true);
         // NOTE: must be sent before fork
-        DEBUG(errs() << "[Master] Sending to " << UserId << ": " << ChildMSQKey << "...");
-        if (CommunicateMSQ->sendMessage(std::to_string(ChildMSQKey), UserId) == -1) {
+        DEBUG(errs() << "[Master] Sending to " << UserID << ": " << ChildMSQKey << "...");
+        if (CommunicateMSQ->sendMessage(std::to_string(ChildMSQKey), UserID) == -1) {
             throw std::runtime_error("[Master] Fail to send slave id to user after (re)open without reuse!");
         }
         DEBUG(errs() << "Done!\n");
@@ -263,13 +265,13 @@ int main(int argc, char **argv) {
         }
         break;
         default: { // Parent process
-            UserWorkerMap[UserId] = {ChildPid, ChildMSQKey};
+            UserWorkerMap[UserID] = {ChildPid, ChildMSQKey};
             // Here we need one guarantee
             // 1. client has got the sent msg
             if (-1 == CommunicateMSQ->recvMessage(CtrlMsg, 11)) {
                 llvm_unreachable("[Master] Fail to receive confirmation!");
             }
-            DEBUG(assert(CtrlMsg == std::to_string(UserId) + ":got"));
+            DEBUG(assert(CtrlMsg == std::to_string(UserID) + ":got"));
             // This has been copied to subprocess, and is not necessary here.
             delete SlaveMSQ;
         }
